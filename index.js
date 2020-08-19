83c9b9d59b59e3840d28cb00b789e7601bc16aaa
Dr Phil <philip.prewitt@gmail.com>
2020-08-19 06:56:28 -0600

    adding email and github username prompts

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const fs = require("fs");
 const userInputs = [
     {
         type: "input",
+        message: "Please enter your email address: ",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Please enter your github username: ",
+        name: "gitusername"
+    },
+    
+    
+    {
+        type: "input",
         message: "Please enter a project title: ",
         name: "title"
     },
@@ -26,7 +38,7 @@ const userInputs = [
     {
         type: "input",
         message: "Who contributed to your project? Did you follow any tutorials or reference any guides?",
-        name: "Credits"
+        name: "credits"
     },
     {
         type: "list",
@@ -52,23 +64,30 @@ const userInputs = [
     {
         type: "input",
         message: "How do you test this project?",
-        name: "tests"
+        name: "test"
     },
 ];
 
 // function to write README file
-.then(function (data) {
+inquirer.prompt(userInputs).then(function (data) {
     let filename = ("README.md");
-   fs.writeFile(fileName, data, function(err) ){
+   fs.writeFile(fileName, data, function(err) {
     if (err) {
         return console.log(err);
       }
     
       console.log("README has been generated!");
-}
+})
 
 // function to initialize program
 function init() {
+    let title = response.title
+    let description = response.description
+    let installation = response.installation
+    let usage = response.usage
+    let credits = response.credits
+    let license = response.license
+    let test = response.test
 
 }
 
