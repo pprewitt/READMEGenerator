b287e8e5fbdc29c84ff8838144c26964d9012807
Dr Phil <philip.prewitt@gmail.com>
2020-08-20 22:01:15 -0600

    async

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const axios = require("axios");
+const util = require('util')
 
-const readMePrompts = [
+const writeFileAsync = util.promisify(fs.writeFile) 
+
+const readMePrompts = () => {
+    return inquirer.prompt([
 {
     type: "input",
     message: "Please enter your email address: ",
@@ -67,40 +71,29 @@ const readMePrompts = [
     name: "test"
 },
 
-]
-writePage();
+]);
 
-async function writePage() {
-  await inquirer.prompt(readMePrompts).then(function(response){
-    let email = response.email
-    let gitusername = response.gitusername
-    let title = response.title
-    let description = response.description
-    let installation = response.installation
-    let usage = response.usage
-    let credits = response.credits
-    let license = response.license
-    let test = response.test
-  });
-} 
+const generateMarkdown = ({email, gitusername, title, description, installation, usage, credits, license, contribute, test }) =>{
+    return `
+    \n \n# ${title}  // Title
+    \n \n ## Description \n ${description} // Description
+    \n \n# Table of Contents \n- [Installation](#installation) \n- // Table Of Contents
+    [Usage](#usage) \n- [Contribution](#contribution) \n- 
+    [Tests](#tests) \n- [License](#license) \n- [Questions](#questions) \n \n  
+    ## Installation \n ${installation} // Installation
+    \n \n## Usage \n ${usage} // Usage
+    \n \n## License \n This application is covered by: ${license} // License Section
+    \n \n## Contribution \n ${contribute} // Contribution 
+    \n \n## Tests \n ${tests} // Tests
+    \n \n## Questions \n If you have any questions feel free to contact: \n \n // Questions & Contact
+    [GitHub](https://github.com/${gitusername}) \n \n Email: [${email}](mailto:${email}); `
+// readme file text
+}
 
-
-// // function to write README file
-// inquirer.prompt(userInputs).then(function (data) {
-//     let filename = ("README.md");
-//    fs.writeFile(fileName, data, function(err) {
-//     if (err) {
-//         return console.log(err);
-//       }
-    
-//       console.log("README has been generated!");
-// })
-
-// // function to initialize program
-// function init() {
+// async function writePage() {
+//   await inquirer.prompt(readMePrompts).then(function(response){
 //     let email = response.email
-    
-//     let gitusername = response.gitusername
+//     let gitusername = response.gitusername 
 //     let title = response.title
 //     let description = response.description
 //     let installation = response.installation
@@ -109,7 +102,36 @@ async function writePage() {
 //     let license = response.license
 //     let test = response.test
 
-// }
+   
+   
+//    fs.writeFile('README.md', data, function(err) {
+//         if (err) {
+//             return console.log(err);
+//           }
+        
+//           console.log("README has been generated!");
+//   });
+// } 
 
-// // function call to initialize program
-// init();
+
+// // function to write README file
+// inquirer.prompt(userInputs).then(function (data) {
+//     let filename = ("README.md");
+   
+// })
+
+// function to initialize program
+const init= async () =>{
+    try {
+        const answers = await readMePrompts();
+        const README = generateMarkdown(answers);
+        await writeFileAsync(README.md, data)
+        console.log("README has been generated!")
+    } catch (error) {
+        console.log (error)
+    }
+    
+}
+
+// function call to initialize program
+init();
